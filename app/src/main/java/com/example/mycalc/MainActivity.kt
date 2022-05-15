f2b79edbc27069168ba420a56df238fcde7ce460
momomo0420-lab <momomo0420.lab+github@gmail.com>
2022-05-15 21:59:45 +0900

    Implemented AppBar.

--- app/src/main/java/com/example/mycalc/MainActivity.kt
+++ app/src/main/java/com/example/mycalc/MainActivity.kt
@@ -2,6 +2,11 @@ package com.example.mycalc
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.example.mycalc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +18,13 @@ class MainActivity : AppCompatActivity() {
 
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //トップアプリバーの設定
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val controller = navHostFragment.findNavController()
+        val configuration = AppBarConfiguration(controller.graph)
+        binding.toolbar.setupWithNavController(controller, configuration)
     }
 
     override fun onDestroy() {
